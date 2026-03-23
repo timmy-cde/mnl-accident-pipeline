1395fcdf8699e0de697fdafd198ad5aff7ca17e1
Timothy Manuel <timothys.manuel@gmail.com>
2026-03-23 23:45:26 +0800

    created terraform

---
+++ terraform/variables.tf
@@ -0,0 +1,34 @@
+variable "credentials" {
+  description = "My Credential"
+  default     = "./keys/mnl-accident-pipeline-key.json"
+}
+
+variable "project" {
+  description = "project name"
+  default     = "mnl-accident-pipeline"
+}
+
+variable "location" {
+  description = "Project Location"
+  default     = "US"
+}
+
+variable "region" {
+  description = "Region"
+  default     = "us-central1"
+}
+
+variable "bq_dataset_name" {
+  description = "My BigQuery Dataset Name"
+  default     = "mnl_accident_pipeline_dataset"
+}
+
+variable "gcs_bucket_name" {
+  description = "My storage bucket name"
+  default     = "mnl_accident_pipeline_bucket"
+}
+
+variable "gcs_storage_class" {
+  description = "Bucket Storage Class"
+  default     = "STANDARD"
+}
