20831097e56240a758c48b2ba08217a1efbc8e38
Timothy Manuel <timothys.manuel@gmail.com>
2026-04-18 01:10:59 +0800

    added dates procedure

---
+++ sql/upsert_dates.sql
@@ -0,0 +1,32 @@
+CREATE OR REPLACE PROCEDURE mnl_accident_pipeline_dataset.upsert_dates()
+BEGIN
+
+  BEGIN TRANSACTION;
+
+  INSERT INTO mnl_accident_pipeline_dataset.dim_dates (date, year, month, day, week, weekday)
+
+  WITH staging AS (
+    SELECT
+      date, 
+      year, 
+      month, 
+      day, 
+      week, 
+      weekday
+    FROM mnl_accident_pipeline_dataset.staging_enriched
+)
+
+  SELECT
+    s.date, 
+    s.year, 
+    s.month, 
+    s.day, 
+    s.week, 
+    s.weekday
+  FROM staging s
+  LEFT JOIN mnl_accident_pipeline_dataset.dim_dates d
+    ON s.date = d.date
+
+COMMIT TRANSACTION;
+
+END;
